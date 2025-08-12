54f2ae58a3369c0dfea0fd0410836e8ce06d7acc
sasou-web <samir@sasou.fr>
2025-08-12 03:40:44 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,17 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 
-const repoName = 'portfolio-samir-lain';
 const isProd = process.env.GITHUB_ACTIONS === 'true';
 
+// Comme tu utilises un domaine custom, pas besoin du /repoName/
+const BASE = '/';
+const SITE = isProd
+  ? 'https://moghrabi.fr/portfolio-samir-lain/'
+  : 'http://localhost:4321';
+
 export default defineConfig({
-  site: isProd
-    ? `https://moghrabi.fr/${repoName}/`
-    : 'http://localhost:4321',
-  base: isProd ? `/${repoName}/` : '/',
+  site: SITE,
+  base: BASE,
   integrations: [tailwind({ applyBaseStyles: false }), mdx()],
-  output: 'static',
+  output: 'static'
 });

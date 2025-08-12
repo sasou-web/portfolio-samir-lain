137a8ee6b081ababfe4892677c31663f78ac3b9e
sasou-web <samir@sasou.fr>
2025-08-12 03:36:53 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,24 +2,14 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 
-// Nom du repo GitHub (important pour GitHub Pages en mode sous-dossier)
 const repoName = 'portfolio-samir-lain';
-
-// On détecte si on est en build sur GitHub Actions
 const isProd = process.env.GITHUB_ACTIONS === 'true';
 
-// BASE = chemin de base pour GitHub Pages
-const BASE = isProd ? `/${repoName}/` : '/';
-
-// SITE = URL complète du site
-// ⚠️ Utilise l'URL GitHub Pages officielle en prod, pas ton domaine custom
-const SITE = isProd
-  ? `https://sasou-web.github.io/${repoName}/`
-  : 'http://localhost:4321';
-
 export default defineConfig({
-  site: SITE,
-  base: BASE,
+  site: isProd
+    ? `https://moghrabi.fr/${repoName}/`
+    : 'http://localhost:4321',
+  base: isProd ? `/${repoName}/` : '/',
   integrations: [tailwind({ applyBaseStyles: false }), mdx()],
-  output: 'static'
+  output: 'static',
 });
